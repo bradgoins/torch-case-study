4ea8183449aed075ea1654bce28779a7b0095fd7
Brad Goins <bradgoins@gmail.com>
2022-03-28 00:21:51 -0500

    refactor for testing

--- api/services/monitorMTA.js
+++ api/services/monitorMTA.js
@@ -1,37 +1,35 @@
-const request = require("request");
+const axios = require("axios");
 const setStatus = require("./setStatus");
 
-function monitorMTA() {
+async function monitorMTA() {
   const requestSettings = {
-    method: "GET",
-    url: "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts.json",
     headers: {
       "x-api-key": process.env.API_KEY,
     },
   };
 
-  request(requestSettings, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      const results = JSON.parse(body);
+  const response = await axios.get("https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts.json", requestSettings);
 
-      const delays = {};
-      results.entity.forEach((entity) => {
-        if (
-          entity.alert["transit_realtime.mercury_alert"].alert_type === "Delays"
-        ) {
-          entity.alert.informed_entity.forEach((e) => {
-            if (e.route_id) {
-              delays[e.route_id] = true;
-            }
-          });
-        }
-      });
+  if (response.status == 200) {
+    const results = response.data;
 
-      setStatus(delays);
-    }
+    const delays = {};
+    results.entity.forEach((entity) => {
+      if (
+        entity.alert["transit_realtime.mercury_alert"].alert_type === "Delays"
+      ) {
+        entity.alert.informed_entity.forEach((e) => {
+          if (e.route_id) {
+            delays[e.route_id] = true;
+          }
+        });
+      }
+    });
 
-    setTimeout(monitorMTA, 60000); // check again after 1 min
-  });
+    setStatus(delays);
+  }
+
+  setTimeout(monitorMTA, 60000); // check again after 1 min
 }
 
 module.exports = monitorMTA;
